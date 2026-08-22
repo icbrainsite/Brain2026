0328ad46560c5f847f912d500bbfbb81c528cfee
icbrainsite <icbrain@nsut.ac.in>
2026-08-22 23:56:07 +0530

    Create dates.js

---
+++ dates.js
@@ -0,0 +1,34 @@
+{
+  "dates": [
+    {
+      "title": "Submission Begins",
+      "date": "2026-08-18",
+      "status": "ongoing"
+    },
+    {
+      "title": "Registration Begins",
+      "date": "2026-09-01",
+      "status": "upcoming"
+    },
+    {
+      "title": "Paper Submission Deadline",
+      "date": "2026-10-25",
+      "status": "upcoming"
+    },
+    {
+      "title": "Acceptance",
+      "date": "2026-11-01",
+      "status": "upcoming"
+    },
+    {
+      "title": "Registration Ends",
+      "date": "2026-11-26",
+      "status": "upcoming"
+    },
+    {
+      "title": "Conference",
+      "date": "2026-12-21",
+      "status": "main-event"
+    }
+  ]
+}
